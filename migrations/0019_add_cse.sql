05ba560663344c888ccc246b8c1c4358b821f475
Slamanna212 <1133401+slamanna212@users.noreply.github.com>
2026-01-22 15:30:05 -0500

    add CSE

---
+++ migrations/0019_add_cse.sql
@@ -0,0 +1,3 @@
+-- Add CSE service
+INSERT INTO services (name, category, check_type, statuspage_id, display_order) VALUES
+    ('CSE', 'msp_tool', 'statuspage', 'https://status.banyanops.com', 14);
